966c9eefa9ae9deb181289eff2714f2ac2a89452
David Ccolcca <draigner143@gmail.com>
2025-04-22 21:16:24 -0500

    Obrero

---
+++ ExamenProgra/Obrero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenProgra
+{
+    internal class Obrero : ITrabajador
+    {
+        public int CalcularProduccion(int produccionMina)
+        {
+            return produccionMina;
+        }
+    }
+}
