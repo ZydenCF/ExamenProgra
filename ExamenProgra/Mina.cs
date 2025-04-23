5fe7022b466e206d1c53284b98e71f96ada1a769
David Ccolcca <draigner143@gmail.com>
2025-04-22 21:26:52 -0500

    Mina

---
+++ ExamenProgra/Mina.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenProgra
+{
+    abstract class Mina
+    {
+        public string Tipo;
+        public int Produccion;
+        public int Costo;
+        public string Estado;
+        public ITrabajador Trabajador;
+        public abstract int GenerarPlata();
+    }
+}
